d75874401abfc74c2f5ee5127c23b3302c4cd58a
lee <mosskimo@gmail.com>
2014-01-05 14:54:28 +0900

    add get_commit_list() : The function makes list from git-log

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import os
+import re
 from subprocess import Popen, PIPE
 
 class Generator:
@@ -15,3 +16,30 @@ class Generator:
         p = Popen(git_log, stdout=PIPE)
         result = p.communicate()
         return result[0]
+
+    def get_commit_list(self):
+        os.chdir(self._path)
+
+        SEPARATOR = "!!!@@"
+        format_string = ""
+        pattern_string = ""
+
+        fields = ["%s", "%an", "%ad"]
+        for f in fields:
+            format_string = format_string + f + SEPARATOR
+            pattern_string = pattern_string + "(.+)" + SEPARATOR
+
+        git_log = ["git", "log", "--pretty=" + format_string]
+        p = Popen(git_log, stdout=PIPE)
+        result, err = p.communicate()
+
+        pattern = re.compile('(.+)'+SEPARATOR + '(.+)' + SEPARATOR + '(.+)' + SEPARATOR)
+        return re.findall(pattern, result)
+
+if __name__ == '__main__':
+    TEST_PATH = "/home/lee/caf/base"
+    gen=Generator(TEST_PATH)
+    commit_list = gen.get_commit_list()
+    print commit_list
+
+
